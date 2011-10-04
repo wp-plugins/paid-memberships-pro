3b72e8170becbf07f59c9bacfafe3e19a4b29a2b
strangerstudios <strangerstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-04 04:43:20 +0000

    1.2.6. Currency Settings. Discount code display bug fix.
    git-svn-id: https://plugins.svn.wordpress.org/paid-memberships-pro/tags/1.2.6@447002 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- pages/invoice.php
+++ pages/invoice.php
@@ -1,5 +1,5 @@
 <?php 
-	global $pmpro_invoice, $pmpro_msg, $pmpro_msgt;
+	global $pmpro_invoice, $pmpro_msg, $pmpro_msgt, $pmpro_currency_symbol;
 	
 	if($pmpro_msg)
 	{
@@ -14,14 +14,20 @@
 		$pmpro_invoice->getUser();
 		$pmpro_invoice->getMembershipLevel();
 	?>
-
+	
 	<h3>Invoice #<?=$pmpro_invoice->code?> on <?=date("F j, Y", $pmpro_invoice->timestamp)?></h3>
 	<a class="pmpro_a-print" href="javascript:window.print()">Print</a>
 	<ul>
 		<li><strong>Account:</strong> <?=$pmpro_invoice->user->display_name?> (<?=$pmpro_invoice->user->user_email?>)</li>
 		<li><strong>Membership Level:</strong> <?=$pmpro_invoice->membership_level->name?></li>
+		<?php if($pmpro_invoice->membership_level->enddate) { ?>
+			<li><strong>Membership Expires:</strong> <?=date("n/j/Y", $pmpro_invoice->membership_level->enddate)?></li>
+		<?php } ?>
+		<?php if($pmpro_invoice->getDiscountCode()) { ?>
+			<li><strong>Discount Code:</strong> <?=$pmpro_invoice->discount_code->code?></li>
+		<?php } ?>
 	</ul>
-
+		
 	<table class="pmpro_invoice" width="100%" cellpadding="0" cellspacing="0" border="0">
 		<thead>
 			<tr>
@@ -45,14 +51,16 @@
 					<?php if($pmpro_invoice->accountnumber) { ?>
 						<?=$pmpro_invoice->cardtype?> ending in <?=last4($pmpro_invoice->accountnumber)?><br />
 						<small>Expiration: <?=$pmpro_invoice->expirationmonth?>/<?=$pmpro_invoice->expirationyear?></small>
+					<?php } elseif($pmpro_invoice->payment_type) { ?>
+						<?=$pmpro_invoice->payment_type?>
 					<?php } ?>
 				</td>
 				<td><?=$pmpro_invoice->membership_level->name?></td>					
 				<td align="center">
 					<?php if($pmpro_invoice->total != '0.00') { ?>
-						$<?=number_format($pmpro_invoice->total, 2)?>
+						<?=$pmpro_currency_symbol?><?=number_format($pmpro_invoice->total, 2)?>
 					<?php } else { ?>
-						<small class="pmpro_grey">$0</small>
+						<small class="pmpro_grey"><?=$pmpro_currency_symbol?>0</small>
 					<?php } ?>		
 				</td>
 			</tr>
