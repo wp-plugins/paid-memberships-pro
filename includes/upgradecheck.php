62e45f808cdc4ae17f0d239d54a57be5f9b5595b
strangerstudios <strangerstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-18 10:57:35 +0000

    1.4.5 Authorize.net updates. New hooks to support pmpro-addon-packages.
    git-svn-id: https://plugins.svn.wordpress.org/paid-memberships-pro/tags/1.4.5@545716 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/upgradecheck.php
+++ includes/upgradecheck.php
@@ -16,10 +16,66 @@ function pmpro_checkForUpgrades()
 	
 	if(!$pmpro_db_version)
 		$pmpro_db_version = pmpro_upgrade_1();	
-	elseif($pmpro_db_version < 1.115)
+	
+	if($pmpro_db_version < 1.115)
 		$pmpro_db_version = pmpro_upgrade_1_1_15();		
-	elseif($pmpro_db_version < 1.23)
-		$pmpro_db_version = pmpro_upgrade_1_2_3();		
+	
+	if($pmpro_db_version < 1.23)
+		$pmpro_db_version = pmpro_upgrade_1_2_3();	
+	
+	if($pmpro_db_version < 1.318)
+		$pmpro_db_version = pmpro_upgrade_1_3_18();
+	
+	if($pmpro_db_version < 1.4)
+		$pmpro_db_version = pmpro_upgrade_1_4();
+		
+	if($pmpro_db_version < 1.42)
+		$pmpro_db_version = pmpro_upgrade_1_4_2();
+}
+
+function pmpro_upgrade_1_4_2()
+{
+	/*
+		Setting the new use_ssl setting.
+		PayPal Website Payments Pro, Authorize.net, and Stripe will default to use ssl.
+		PayPal Express and the test gateway (no gateway) will default to not use ssl.
+	*/
+	$gateway = pmpro_getOption("gateway");
+	if($gateway == "paypal" || $gateway == "authorizenet" || $gateway == "stripe")
+		pmpro_setOption("use_ssl", 1);
+	else
+		pmpro_setOption("use_ssl", 0);
+		
+	pmpro_setOption("db_version", "1.42");
+	return 1.42;
+}
+
+function pmpro_upgrade_1_4()
+{
+	global $wpdb;
+	$wpdb->hide_errors();
+	$wpdb->pmpro_membership_levels = $wpdb->prefix . 'pmpro_membership_levels';
+	
+	//confirmation message
+	$sqlQuery = "
+		ALTER TABLE  `" . $wpdb->pmpro_membership_levels . "` ADD  `confirmation` LONGTEXT NOT NULL AFTER  `description`
+	";
+	$wpdb->query($sqlQuery);
+	
+	pmpro_setOption("db_version", "1.4");
+	return 1.4;
+}
+
+function pmpro_upgrade_1_3_18()
+{
+	//setting new email settings defaults
+	pmpro_setOption("email_admin_checkout", "1");
+	pmpro_setOption("email_admin_changes", "1");
+	pmpro_setOption("email_admin_cancels", "1");
+	pmpro_setOption("email_admin_billing", "1");
+
+	pmpro_setOption("db_version", "1.318");	
+	return 1.318;
 }
 
 function pmpro_upgrade_1_2_3()
@@ -233,7 +289,7 @@ function pmpro_upgrade_1()
 	pmpro_setOption("from_email", $from_email);
 	
 	$from_name = "WordPress";
-	pmpro_setOption("from_name", $from_name);
+	pmpro_setOption("from_name", $from_name);		
 	
 	pmpro_setOption("tospage", "");			
 	
