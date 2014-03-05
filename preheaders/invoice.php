98d57507c51184f6131df47e381c702986ea7e8f
strangerstudios <strangerstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-05 23:33:29 +0000

    v1.7.8 - Bug fixes, new hooks and filters, order report filters, Brazilian Portuguese translation, addon updates.
    git-svn-id: https://plugins.svn.wordpress.org/paid-memberships-pro/tags/1.7.8@870296 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- preheaders/invoice.php
+++ preheaders/invoice.php
@@ -1,23 +1,33 @@
 <?php
 	global $current_user, $pmpro_invoice;
 	
-	//get invoice from DB
-	$invoice_code = $_REQUEST['invoice'];
-		
-	if(!$invoice_code)
+	if(!is_user_logged_in())
 	{
-		if(PMPRO_FLAG_NO_INVOICE_REDIRECT == false)
-			wp_redirect(pmpro_url("account"));	//no code
+		wp_redirect(pmpro_url("account"));
+		exit;
 	}
+	
+	//get invoice from DB
+	if(!empty($_REQUEST['invoice']))
+		$invoice_code = $_REQUEST['invoice'];
 	else
+		$invoice_code = NULL;
+	
+	if(!empty($invoice_code))	
 	{
 		$pmpro_invoice = new MemberOrder($invoice_code);
+		
 		//var_dump($pmpro_invoice);
 		if(!$pmpro_invoice->id)
+		{
 			wp_redirect(pmpro_url("account")); //no match
+			exit;
+		}
 		
 		//make sure they have permission to view this
 		if(!current_user_can("administrator") && $current_user->ID != $pmpro_invoice->user_id)
-			wp_redirect(pmpro_url("account")); //no permission				
+		{
+			wp_redirect(pmpro_url("account")); //no permission
+			exit;
+		}
 	}
-?>
