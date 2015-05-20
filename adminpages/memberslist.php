9c3a47f386c7c3067fcd58b58b716cff6b682549
strangerstudios <strangerstudios@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-20 19:21:04 +0000

    Fixes issue introduced in 1.8.4 where levels won't show if they haven't been reordered.
    git-svn-id: https://plugins.svn.wordpress.org/paid-memberships-pro/tags/1.8.4.1@1164429 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- adminpages/memberslist.php
+++ adminpages/memberslist.php
@@ -1,45 +1,35 @@
 <?php
+	//only admins can get this
+	if(!function_exists("current_user_can") || (!current_user_can("manage_options") && !current_user_can("pmpro_memberslist")))
+	{
+		die(__("You do not have permissions to perform this action.", "pmpro"));
+	}
+
 	//vars
 	global $wpdb;
 	if(isset($_REQUEST['s']))
-		$s = $_REQUEST['s'];
+		$s = trim($_REQUEST['s']);
 	else
 		$s = "";
-	
+
 	if(isset($_REQUEST['l']))
 		$l = $_REQUEST['l'];
 	else
 		$l = false;
-?>
-<div class="wrap pmpro_admin">	
-	<div class="pmpro_banner">		
-		<a class="pmpro_logo" title="Paid Memberships Pro - Membership Plugin for WordPress" target="_blank" href="<?php echo pmpro_https_filter("http://www.paidmembershipspro.com")?>"><img src="<?php echo PMPRO_URL?>/images/PaidMembershipsPro.gif" width="350" height="45" border="0" alt="Paid Memberships Pro(c) - All Rights Reserved" /></a>
-		<div class="pmpro_tagline">Membership Plugin for WordPress</div>
-		
-		<div class="pmpro_meta"><a href="<?php echo pmpro_https_filter("http://www.paidmembershipspro.com")?>">Plugin Support</a> | <a href="http://www.paidmembershipspro.com/forums/">User Forum</a> | <strong>Version <?php echo PMPRO_VERSION?></strong></div>
-	</div>
-	<br style="clear:both;" />
-	
-	<?php
-		//include(pmpro_https_filter("http://www.paidmembershipspro.com/notifications/?v=" . PMPRO_VERSION));
-	?>
-	<div id="pmpro_notifications">
-	</div>
-	<script>
-		jQuery.get('<?php echo pmpro_https_filter("http://www.paidmembershipspro.com/notifications/?v=" . PMPRO_VERSION)?>', function(data) {
-		  jQuery('#pmpro_notifications').html(data);		 
-		});
-	</script>
 
-	<form id="posts-filter" method="get" action="">	
+	require_once(dirname(__FILE__) . "/admin_header.php");
+?>
+
+	<form id="posts-filter" method="get" action="">
 	<h2>
-		Members Report
-		<small>(<a target="_blank" href="<?php echo PMPRO_URL?>/adminpages/memberslist-csv.php?s=<?php echo $s?>&l=<?php echo $l?>">Export to CSV</a>)</small>
-	</h2>		
+		<?php _e('Members List', 'pmpro');?>
+		<a target="_blank" href="<?php echo admin_url('admin-ajax.php');?>?action=memberslist_csv&s=<?php echo $s?>&l=<?php echo $l?>" class="add-new-h2"><?php _e('Export to CSV', 'pmpro');?></a>
+	</h2>
 	<ul class="subsubsub">
-		<li>			
-			Show <select name="l" onchange="jQuery('#posts-filter').submit();">
-				<option value="" <?php if(!$l) { ?>selected="selected"<?php } ?>>All Levels</option>
+		<li>
+			<?php _e('Show', 'pmpro');?>
+			<select name="l" onchange="jQuery('#posts-filter').submit();">
+				<option value="" <?php if(!$l) { ?>selected="selected"<?php } ?>><?php _e('All Levels', 'pmpro');?></option>
 				<?php
 					$levels = $wpdb->get_results("SELECT id, name FROM $wpdb->pmpro_membership_levels ORDER BY name");
 					foreach($levels as $level)
@@ -49,140 +39,228 @@
 				<?php
 					}
 				?>
-			</select>			
+				<option value="cancelled" <?php if($l == "cancelled") { ?>selected="selected"<?php } ?>><?php _e('Cancelled Members', 'pmpro');?></option>
+				<option value="expired" <?php if($l == "expired") { ?>selected="selected"<?php } ?>><?php _e('Expired Members', 'pmpro');?></option>
+				<option value="oldmembers" <?php if($l == "oldmembers") { ?>selected="selected"<?php } ?>><?php _e('Old Members', 'pmpro');?></option>
+			</select>
 		</li>
 	</ul>
 	<p class="search-box">
-		<label class="hidden" for="post-search-input">Search Members:</label>
-		<input type="hidden" name="page" value="pmpro-memberslist" />		
+		<label class="hidden" for="post-search-input"><?php _e('Search Members', 'pmpro');?>:</label>
+		<input type="hidden" name="page" value="pmpro-memberslist" />
 		<input id="post-search-input" type="text" value="<?php echo $s?>" name="s"/>
-		<input class="button" type="submit" value="Search Members"/>
+		<input class="button" type="submit" value="<?php _e('Search Members', 'pmpro');?>"/>
 	</p>
-	<?php 
+	<?php
 		//some vars for the search
 		if(isset($_REQUEST['pn']))
-			$pn = $_REQUEST['pn'];
+			$pn = intval($_REQUEST['pn']);
 		else
 			$pn = 1;
-			
+
 		if(isset($_REQUEST['limit']))
-			$limit = $_REQUEST['limit'];
+			$limit = intval($_REQUEST['limit']);
 		else
 			$limit = 15;
-		
+
 		$end = $pn * $limit;
-		$start = $end - $limit;				
-					
+		$start = $end - $limit;
+
 		if($s)
 		{
-			$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->usermeta um ON u.ID = um.user_id LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id WHERE mu.membership_id > 0 AND (u.user_login LIKE '%$s%' OR u.user_email LIKE '%$s%' OR um.meta_value LIKE '%$s%') ";
-		
-			if($l)
-				$sqlQuery .= " AND mu.membership_id = '" . $l . "' ";					
-				
-			$sqlQuery .= "GROUP BY u.ID ORDER BY user_registered DESC LIMIT $start, $limit";
+			$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->usermeta um ON u.ID = um.user_id LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id ";
+
+			if($l == "oldmembers" || $l == "expired" || $l == "cancelled")
+				$sqlQuery .= " LEFT JOIN $wpdb->pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' ";
+
+			$sqlQuery .= " WHERE mu.membership_id > 0 AND (u.user_login LIKE '%" . esc_sql($s) . "%' OR u.user_email LIKE '%" . esc_sql($s) . "%' OR um.meta_value LIKE '%" . esc_sql($s) . "%') ";
+
+			if($l == "oldmembers")
+				$sqlQuery .= " AND mu.status <> 'active' AND mu2.status IS NULL ";
+			elseif($l == "expired")
+				$sqlQuery .= " AND mu.status = 'expired' AND mu2.status IS NULL ";
+			elseif($l == "cancelled")
+				$sqlQuery .= " AND mu.status IN('cancelled', 'admin_cancelled') AND mu2.status IS NULL ";
+			elseif($l)
+				$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id = '" . esc_sql($l) . "' ";
+			else
+				$sqlQuery .= " AND mu.status = 'active' ";
+
+			$sqlQuery .= "GROUP BY u.ID ";
+
+			if($l == "oldmembers" || $l == "expired" || $l == "cancelled")
+				$sqlQuery .= "ORDER BY enddate DESC ";
+			else
+				$sqlQuery .= "ORDER BY u.user_registered DESC ";
+
+			$sqlQuery .= "LIMIT $start, $limit";
 		}
 		else
 		{
-			$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id ";
-			$sqlQuery .= "WHERE mu.membership_id > 0 ";
-			if($l)
-				$sqlQuery .= " AND mu.membership_id = '" . $l . "' ";										
-			$sqlQuery .= "ORDER BY user_registered DESC LIMIT $start, $limit";
+			$sqlQuery = "SELECT SQL_CALC_FOUND_ROWS u.ID, u.user_login, u.user_email, UNIX_TIMESTAMP(u.user_registered) as joindate, mu.membership_id, mu.initial_payment, mu.billing_amount, mu.cycle_period, mu.cycle_number, mu.billing_limit, mu.trial_amount, mu.trial_limit, UNIX_TIMESTAMP(mu.startdate) as startdate, UNIX_TIMESTAMP(mu.enddate) as enddate, m.name as membership FROM $wpdb->users u LEFT JOIN $wpdb->pmpro_memberships_users mu ON u.ID = mu.user_id LEFT JOIN $wpdb->pmpro_membership_levels m ON mu.membership_id = m.id";
+
+			if($l == "oldmembers" || $l == "expired" || $l == "cancelled")
+				$sqlQuery .= " LEFT JOIN $wpdb->pmpro_memberships_users mu2 ON u.ID = mu2.user_id AND mu2.status = 'active' ";
+
+			$sqlQuery .= " WHERE mu.membership_id > 0  ";
+
+			if($l == "oldmembers")
+				$sqlQuery .= " AND mu.status <> 'active' AND mu2.status IS NULL ";
+			elseif($l == "expired")
+				$sqlQuery .= " AND mu.status = 'expired' AND mu2.status IS NULL ";
+			elseif($l == "cancelled")
+				$sqlQuery .= " AND mu.status IN('cancelled', 'admin_cancelled') AND mu2.status IS NULL ";
+			elseif($l)
+				$sqlQuery .= " AND mu.status = 'active' AND mu.membership_id = '" . esc_sql($l) . "' ";
+			else
+				$sqlQuery .= " AND mu.status = 'active' ";
+			$sqlQuery .= "GROUP BY u.ID ";
+
+			if($l == "oldmembers" || $l == "expired" || $l == "cancelled")
+				$sqlQuery .= "ORDER BY enddate DESC ";
+			else
+				$sqlQuery .= "ORDER BY u.user_registered DESC ";
+
+			$sqlQuery .= "LIMIT $start, $limit";
 		}
-						
+
+		$sqlQuery = apply_filters("pmpro_members_list_sql", $sqlQuery);
+
 		$theusers = $wpdb->get_results($sqlQuery);
 		$totalrows = $wpdb->get_var("SELECT FOUND_ROWS() as found_rows");
-		
+
 		if($theusers)
 		{
-			$initial_payments = pmpro_calculateInitialPaymentRevenue($s, $l);
-			$recurring_payments = pmpro_calculateRecurringRevenue($s, $l);
-		?>
-		<p class="clear"><?php echo strval($totalrows)?> members found. These members have paid <strong>$<?php echo number_format($initial_payments)?> in initial payments</strong> and will generate an estimated <strong>$<?php echo number_format($recurring_payments)?> in revenue over the next year</strong>, or <strong>$<?php echo number_format($recurring_payments/12)?>/month</strong>. <span class="pmpro_lite">(This estimate does not take into account trial periods or billing limits.)</span></p>
-		<?php
-		}		
+			$calculate_revenue = apply_filters("pmpro_memberslist_calculate_revenue", false);
+			if($calculate_revenue)
+			{
+				$initial_payments = pmpro_calculateInitialPaymentRevenue($s, $l);
+				$recurring_payments = pmpro_calculateRecurringRevenue($s, $l);
+				?>
+				<p class="clear"><?php echo strval($totalrows)?> members found. These members have paid <strong>$<?php echo number_format($initial_payments)?> in initial payments</strong> and will generate an estimated <strong>$<?php echo number_format($recurring_payments)?> in revenue over the next year</strong>, or <strong>$<?php echo number_format($recurring_payments/12)?>/month</strong>. <span class="pmpro_lite">(This estimate does not take into account trial periods or billing limits.)</span></p>
+				<?php
+			}
+			else
+			{
+			?>
+			<p class="clear"><?php printf(__("%d members found.", "pmpro"), $totalrows);?></span></p>
+			<?php
+			}
+		}
 	?>
 	<table class="widefat">
 		<thead>
 			<tr class="thead">
-				<th>ID</th>
-				<th>Username</th>
-				<th>First&nbsp;Name</th>
-				<th>Last&nbsp;Name</th>
-				<th>Email</th>
-				<th>Billing Address</th>
-				<th>Membership</th>	
-				<th>Fee</th>
-				<th>Joined</th>
-				<th>Expires</th>
+				<th><?php _e('ID', 'pmpro');?></th>
+				<th><?php _e('Username', 'pmpro');?></th>
+				<th><?php _e('First&nbsp;Name', 'pmpro');?></th>
+				<th><?php _e('Last&nbsp;Name', 'pmpro');?></th>
+				<th><?php _e('Email', 'pmpro');?></th>
+				<?php do_action("pmpro_memberslist_extra_cols_header", $theusers);?>
+				<th><?php _e('Billing Address', 'pmpro');?></th>
+				<th><?php _e('Membership', 'pmpro');?></th>
+				<th><?php _e('Fee', 'pmpro');?></th>
+				<th><?php _e('Joined', 'pmpro');?></th>
+				<th>
+					<?php
+						if($l == "oldmembers")
+							_e('Ended', 'pmpro');
+						else
+							_e('Expires', 'pmpro');
+					?>
+				</th>
 			</tr>
 		</thead>
-		<tbody id="users" class="list:user user-list">	
-			<?php	
-				$count = 0;							
-				foreach($theusers as $theuser)
+		<tbody id="users" class="list:user user-list">
+			<?php
+				$count = 0;
+				foreach($theusers as $auser)
 				{
 					//get meta
-					$sqlQuery = "SELECT meta_key as `key`, meta_value as `value` FROM $wpdb->usermeta WHERE $wpdb->usermeta.user_id = '" . $theuser->ID . "'";					
-					$metavalues = pmpro_getMetavalues($sqlQuery);																		
+					$theuser = get_userdata($auser->ID);
 					?>
 						<tr <?php if($count++ % 2 == 0) { ?>class="alternate"<?php } ?>>
 							<td><?php echo $theuser->ID?></td>
-							<td>
+							<td class="username column-username">
 								<?php echo get_avatar($theuser->ID, 32)?>
-								<strong><a href="user-edit.php?user_id=<?php echo $theuser->ID?>"><?php echo $theuser->user_login?></a></strong>
+								<strong>
+									<?php
+										$userlink = '<a href="user-edit.php?user_id=' . $theuser->ID . '">' . $theuser->user_login . '</a>';
+										$userlink = apply_filters("pmpro_members_list_user_link", $userlink, $theuser);
+										echo $userlink;
+									?>
+								</strong>
+								<br />
+								<?php
+									// Set up the hover actions for this user
+									$actions = apply_filters( 'pmpro_memberslist_user_row_actions', array(), $theuser );
+									$action_count = count( $actions );
+									$i = 0;
+									if($action_count)
+									{
+										$out = '<div class="row-actions">';
+										foreach ( $actions as $action => $link ) {
+											++$i;
+											( $i == $action_count ) ? $sep = '' : $sep = ' | ';
+											$out .= "<span class='$action'>$link$sep</span>";
+										}
+										$out .= '</div>';
+										echo $out;
+									}
+								?>
 							</td>
-							<td><?php echo $metavalues->first_name?></td>
-							<td><?php echo $metavalues->last_name?></td>
+							<td><?php echo $theuser->first_name?></td>
+							<td><?php echo $theuser->last_name?></td>
 							<td><a href="mailto:<?php echo $theuser->user_email?>"><?php echo $theuser->user_email?></a></td>
+							<?php do_action("pmpro_memberslist_extra_cols_body", $theuser);?>
 							<td>
-								<?php echo trim($metavalues->pmpro_bfirstname . " " . $metavalues->pmpro_blastname);?><br />
-								<?php echo $metavalues->pmpro_baddress1; ?><br />
-								<?php if(!empty($metavalues->pmpro_baddress2)) echo $metavalues->pmpro_baddress2 . "<br />"; ?>										
-								<?php if($metavalues->pmpro_bcity && $metavalues->pmpro_bstate) { ?>
-									<?php echo $metavalues->pmpro_bcity?>, <?php echo $metavalues->pmpro_bstate?> <?php echo $metavalues->pmpro_bzipcode?><br />												
-								<?php } ?>
-								<?php echo formatPhone($metavalues->pmpro_bphone)?>
+								<?php
+									echo pmpro_formatAddress(trim($theuser->pmpro_bfirstname . " " . $theuser->pmpro_blastname), $theuser->pmpro_baddress1, $theuser->pmpro_baddress2, $theuser->pmpro_bcity, $theuser->pmpro_bstate, $theuser->pmpro_bzipcode, $theuser->pmpro_bcountry, $theuser->pmpro_bphone);
+								?>
 							</td>
-							<td><?php echo $theuser->membership?></td>	
+							<td><?php echo $auser->membership?></td>
 							<td>
-								<?php if($theuser->billing_amount > 0) { ?>
-									$<?php echo $theuser->billing_amount?>/<?php echo $theuser->cycle_period?>
-								<?php } else { ?>
+								<?php if((float)$auser->initial_payment > 0) { ?>
+									<?php echo pmpro_formatPrice($auser->initial_payment);?>
+								<?php } ?>
+								<?php if((float)$auser->initial_payment > 0 && (float)$auser->billing_amount > 0) { ?>+<br /><?php } ?>
+								<?php if((float)$auser->billing_amount > 0) { ?>
+									<?php echo pmpro_formatPrice($auser->billing_amount);?>/<?php if($auser->cycle_number > 1) { echo $auser->cycle_number . " " . $auser->cycle_period . "s"; } else { echo $auser->cycle_period; } ?>
+								<?php } ?>
+								<?php if((float)$auser->initial_payment <= 0 && (float)$auser->billing_amount <= 0) { ?>
 									-
 								<?php } ?>
-							</td>						
-							<td><?php echo date("m/d/Y", $theuser->joindate)?></td>
+							</td>
+							<td><?php echo date(get_option("date_format"), strtotime($theuser->user_registered, current_time("timestamp")))?></td>
 							<td>
-								<?php 
-									if($theuser->enddate) 
-										echo date("m/d/Y", $theuser->enddate);
+								<?php
+									if($auser->enddate)
+										echo apply_filters("pmpro_memberslist_expires_column", date(get_option('date_format'), $auser->enddate), $auser);
 									else
-										echo "Never";
+										echo __(apply_filters("pmpro_memberslist_expires_column", "Never", $auser), "pmpro");
 								?>
 							</td>
 						</tr>
 					<?php
 				}
-				
+
 				if(!$theusers)
 				{
 				?>
 				<tr>
-					<td colspan="9"><p>No members found. <?php if($l) { ?><a href="?page=pmpro-memberslist&s=<?php echo $s?>">Search all levels</a>.<?php } ?></p></td>
+					<td colspan="9"><p><?php _e("No members found.", "pmpro");?> <?php if($l) { ?><a href="?page=pmpro-memberslist&s=<?php echo $s?>"><?php _e("Search all levels", "pmpro");?></a>.<?php } ?></p></td>
 				</tr>
 				<?php
 				}
-			?>		
+			?>
 		</tbody>
 	</table>
 	</form>
-	
+
 	<?php
-	echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, home_url("/wp-admin/admin.php?page=pmpro-memberslist&s=" . urlencode($s)), "&l=$l&limit=$limit&pn=");
+	echo pmpro_getPaginationString($pn, $totalrows, $limit, 1, get_admin_url(NULL, "/admin.php?page=pmpro-memberslist&s=" . urlencode($s)), "&l=$l&limit=$limit&pn=");
 	?>
-	
-</div>
+
 <?php
+	require_once(dirname(__FILE__) . "/admin_footer.php");
 ?>
